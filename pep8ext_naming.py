2d73b3be93ffeae8798e50baaaa00b36dd90021d
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2017-12-31 06:52:12 -0600

    Bump version and add release notes for 0.5.0

--- pep8ext_naming.py
+++ pep8ext_naming.py
@@ -12,7 +12,7 @@ try:
 except ImportError:
     from flake8.util import ast, iter_child_nodes
 
-__version__ = '0.4.1'
+__version__ = '0.5.0'
 
 LOWERCASE_REGEX = re.compile(r'[_a-z][_a-z0-9]*$')
 UPPERCASE_REGEX = re.compile(r'[_A-Z][_A-Z0-9]*$')
