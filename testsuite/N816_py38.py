8ed423da225e3c556896e095ff456d1650b196b1
5j9 <5j9@users.noreply.github.com>
2020-03-09 18:52:41 +0330

    implement visitor method for NamedExpr
    fixes #139

---
+++ testsuite/N816_py38.py
@@ -0,0 +1,5 @@
+# python_version >= '3.8'
+#: Okay
+lambda f: (TheName := namedtuple('TheName', 'a b c')), TheName
+#: N816:1:15:
+lambda line: (BaD_WaLRuS := re.match(pattern, line)) and BaD_WaLRuS.group(1)
