0dfb6eed9db034c12ef5b4040e28a9bc20bc5979
Stavros Ntentos <133706+stdedos@users.noreply.github.com>
2022-12-16 17:54:10 +0200

    `extras_require` does not belong in this PR
    Signed-off-by: Stavros Ntentos <133706+stdedos@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
     package_dir={'': 'src'},
     py_modules=['pep8ext_naming'],
     install_requires=['flake8>=5.0.0'],
-    extras_require={"dev": ["tox"]},
     zip_safe=False,
     python_requires='>=3.7',
     entry_points={
