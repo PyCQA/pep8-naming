0ebc7c9cf7cf8fa23d70ede50741716c4692a704
Jon Parise <jon@indelible.org>
2025-03-25 07:43:19 -0400

    Revert "Import typing.{List,Tuple} for Python 3.8"
    This reverts commit 8986e792defb4d52d7e00969485c5d2eb5feb0a4.

--- src/pep8ext_naming.py
+++ src/pep8ext_naming.py
@@ -6,7 +6,6 @@ from collections.abc import Iterable
 from fnmatch import fnmatchcase
 from functools import partial
 from itertools import chain
-from typing import List, Tuple
 
 from flake8 import style_guide
 
@@ -35,8 +34,8 @@ FUNC_NODES = (ast.FunctionDef, ast.AsyncFunctionDef)
 class BaseASTCheck:
     """Base for AST Checks."""
 
-    all: List['BaseASTCheck'] = []
-    codes: Tuple[str, ...]
+    all: list['BaseASTCheck'] = []
+    codes: tuple[str, ...]
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
